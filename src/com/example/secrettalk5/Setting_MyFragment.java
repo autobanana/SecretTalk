d311235ef6c62a38906f1aa47361f9c5f88d0d6d
Vicky <Vicky@asus-pc>
2014-06-24 23:08:13 +0800

    logo icon done

--- src/com/example/secrettalk5/Setting_MyFragment.java
+++ src/com/example/secrettalk5/Setting_MyFragment.java
@@ -12,25 +12,20 @@ public class Setting_MyFragment extends Fragment {
 	int mNum; //頁號
     public static Setting_MyFragment newInstance( int num) {
     	Setting_MyFragment fragment = new Setting_MyFragment();
-        // Supply num input as an argument.
-//        Bundle args = new Bundle();
-//        args.putInt( "num" , num);
-//        fragment.setArguments(args);
         return fragment;
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super .onCreate(savedInstanceState);
-        //這裡我只是簡單的用num區別標籤，其實具體應用中可以使用真實的fragment對象來作為葉片
-       // mNum = getArguments() != null ? getArguments().getInt( "num" ) : 1 ;
     }
     /**為Fragment加載佈局時調用**/
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
                                                                                                                                                                                                                                                                                                                       
         View view = inflater.inflate(R.layout.fragment_setting_my,  container,false );
-        //TextView tv = (TextView) view.findViewById(R.id.textView1);
-        //tv.setText( "fragment+" + mNum);
+        
+        
+        
         return view;
     }
 
